4fcc003d308d0adff13b02ddbc73312744d1b423
Ben Lusamba <benlusamba@gmail.com>
2018-05-08 17:06:23 -0400

    Curl Automation

--- conv.py
+++ conv.py
@@ -6,11 +6,12 @@
 #In the relevant directory, run the following script to convert to CSV (using Pandas):
 
 import pandas as pd
-df_json_raw = pd.read_json('feed.json')
+
+df_json_raw = pd.read_json('data.json')
 
 df_json = df_json_raw.apply( lambda x: pd.Series([x[0]['title'],x[0]['description'],x[0]['publishedAt'],x[0]['source']]), axis = 1 ) # Add variables as desired e.g. 'source'
 df_json.columns=['Title','Description','Published At','Source']               # Label columbs for csv file, to reflect variables (format as desired)
 
 print (df_json)                                       # Show file in workspace
 
-df_json.to_csv('feed.csv')                            # Export json file as .csv
+df_json.to_csv('breath.csv')                            # Export json file as .csv
---
+++ req.py
@@ -0,0 +1,36 @@
+# Automated news retrieval using news api
+# Skipping need for a "curl" request
+
+import pandas as pd
+import numpy as np
+import urllib.request, json, simplejson, csv, requests
+
+# define parameters, including API KEY
+params = (
+    ('sources', 'the-new-york-times,the-washington-post,the-wall-street-journal,bbc-news,cnn'),
+    ('apiKey', 'e76a07388cad49b49075abced2862f3d'),
+)
+
+response = requests.get('https://newsapi.org/v2/top-headlines', params=params)
+
+data = response.json()
+
+f = open('data.json', 'w')
+simplejson.dump(data, f)
+f.close()
+
+data = pd.read_json('data.json', lines=True)
+
+df_json_raw = pd.read_json('data.json')
+
+# Add variables as desired e.g. 'source'
+df_json = df_json_raw.apply( lambda x: pd.Series([x[0]['title'],x[0]['description'],x[0]['publishedAt'],x[0]['source']]), axis = 1 )
+
+ # Label columbs for csv file, to reflect variables (format as desired)
+df_json.columns=['Title','Description','Published At','Source']
+
+#export as csv
+df_json.to_csv('feed.csv')
+
+# Show file in workspace
+print (data)
