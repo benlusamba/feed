9a7e184e2597e1332fa8eb2767135f7c7491e972
Ben <benlusamba@gmail.com>
2019-02-12 05:08:05 -0500

    directory changes

--- req.py
+++ req.py
@@ -12,33 +12,39 @@ import json as simplejson
 import datetime
 import os
 import os.path
+import datetime
+from datetime import datetime
+import time 
 
+start = time.time()
 # define parameters, including API KEY
 params = (
     ('sources', 'the-new-york-times,the-washington-post,the-wall-street-journal,bbc-news,cnn'),
-    ('apiKey', '*******************'),
+    ('apiKey', '68970^ahshifr'),
 )
 
 response = requests.get('https://newsapi.org/v2/top-headlines', params=params)
 
 data = response.json()
 
+print(response.text)
 # write response as JSON file to enable parsing
-f = open('data.json', 'w')
+f = open('./source/data.json', 'w')
 simplejson.dump(data, f)
 f.close()
 
 #parse JSON file using pandas
-data = pd.read_json('data.json', lines=True)
+data = pd.read_json('./source/data.json', lines=True)
 
-df_json_raw = pd.read_json('data.json')
+df_json_raw = pd.read_json('./source/data.json')
 
 # Add variables as desired e.g. 'source'
 #df_json = df_json_raw.apply( lambda x: pd.Series([x[0]['title'],x[0]['description'],x[0]['publishedAt'],x[0]['source']]), axis = 1 )
 #df_json = df_json_raw.apply( lambda x: pd.Series([x[0]['title'],x[0]['description']]), axis = 1 )
 
 df_json = df_json_raw.apply( lambda x: pd.Series([x[2]['description'],x[2]['title']]), axis = 1)
-
+datadf = pd.DataFrame(data['articles'])
+print(datadf)
 # Label columns for csv file
 #df_json.columns=['Title','Description','Published At','Source']
 df_json.columns=['Description', 'Title']
@@ -46,16 +52,20 @@ df_json.columns=['Description', 'Title']
 #export as csv, respect location and time conventions:
 datestring = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
 
-df_json.to_csv('C:/Users/BELUSA/Documents/Projects/alt/feed/feed.csv')
+archive = './archive'
+df_json.to_csv(os.path.join(archive,r'newsfeed.txt'))
 #path = 'C:/Users/BELUSA/Documents/Projects/alt/feed'
 #df_json.to_csv(os.path.join(path,r'feed_'+datestring+'.csv'))
 
 
 # Show file in workspace
 print(data)
-print(df_json)
+print (df_json)
 
 print('')
 
 print('Word Frequency Count')
 import common
+
+end = time.time()
+print (f"Executed in {np.round(end-start)} seconds")
